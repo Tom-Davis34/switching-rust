c9601c5a35deecd6c20a375b43885733014eb53f
Tom Davis <tom.davis@transpower.co.nz>
2023-11-27 17:25:23 +1300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
+#![cfg(feature = "generic_const_exprs")]
 
 pub mod matrix_builder;
 pub mod runge_kutta;
 pub mod traits;
 mod matrix_utils;
 pub mod butcher_tableau;
+mod butcher_tableau_2;
 
 fn main() {
 
